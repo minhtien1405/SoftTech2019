833b3020bfb2a2aa086e39c6225e3be14f6403f9
Tien Hoang <hoangleminhtien@gmail.com>
2019-11-15 09:03:48 +0100

    Adding login function

--- emailProject/src/app/app-routing.module.ts
+++ emailProject/src/app/app-routing.module.ts
@@ -1,11 +1,38 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./auth/login/login.component";
+import { EmailPageComponent } from "./email-page/email-page.component";
+import { RegistrationComponent } from "./auth/registration/registration.component";
+import { HomeComponent } from "./home/home.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegistrationComponent
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "email",
+    component: EmailPageComponent
+  },
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+export const routingComponents = [LoginComponent, EmailPageComponent];
--- emailProject/src/app/app.module.ts
+++ emailProject/src/app/app.module.ts
@@ -5,8 +5,9 @@ import { FormsModule } from "@angular/forms";
 import { environment } from "../environments/environment";
 import { AngularFireModule } from "angularfire2";
 import { AngularFirestoreModule } from "angularfire2/firestore";
+import { AngularFireAuthModule } from "@angular/fire/auth";
 
-import { AppRoutingModule } from "./app-routing.module";
+import { AppRoutingModule, routingComponents } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { EmailsComponent } from "./components/emails/emails.component";
 
@@ -14,20 +15,30 @@ import { EmailService } from "./services/email.service";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { SendEmailComponent } from "./components/send-email/send-email.component";
 import { fromPromise } from "rxjs/internal-compatibility";
+import { EmailPageComponent } from "./email-page/email-page.component";
+import { RegistrationComponent } from "./auth/registration/registration.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmailsComponent,
     NavbarComponent,
-    SendEmailComponent
+    SendEmailComponent,
+    routingComponents,
+    EmailPageComponent,
+    RegistrationComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase, "emailProject"),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [EmailService],
   bootstrap: [AppComponent]
